063464a3e639b46f3e7ccbc24e3072fa15665627
Kim_Yul <belbet01@kookmin.ac.kr>
2020-06-26 21:27:50 +0900

    Issue #4 Copy the code to use

---
+++ calc/fig.py
@@ -0,0 +1,19 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
+    x = 'nonexistent'
+    y = 'nonexistent'
+    if '' not in [a, b]:
+        a, b = int(a), int(b)
+	x = a + b
+	y = a * b
+    response_body = html+"Addition is {} and Multiplication is {}".format(x, y)
+    start_response('200 OK', [
+	('Content-Type', 'text/html'),
+	('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
---
+++ calc/template.py
@@ -0,0 +1,12 @@
+html = b"""
+<html>
+    <body>
+	<form action="">
+	    Write down the two numbers you want to add and multiply.<br><br>
+	    number 1 = <input type="number" name="a"><br><br>
+	    number 2 = <input tyoe="number" name="b"><br><br>
+            <input type="submit"><br><br>
+	</form>
+    </body>
+</html>
+"""
